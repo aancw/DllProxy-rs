ecc236902aff688dfb6dbbf37d267e72f71235c6
Aan <dalang@petruknisme.com>
2022-10-19 07:21:13 +0000

    Add vswhere and vs.cmd for dll auto compiling feature

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,48 @@
+// Copyright (c) 2022 Petruknisme
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+extern crate clap;
+extern crate colored;
+
+use clap::Parser;
+use colored::Colorize;
 use std::process;
 use std::io::{self, Write};
 use pelite::{FileMap, Wrap, PeFile};
 
+
+#[derive(Parser)]
+#[clap(name = "Spring4shell PoC")]
+#[clap(author = "Petruknisme <me@petruknisme.com>")]
+#[clap(version = "1.0")]
+#[clap(about = "Spring 4 Shell Proof of Concept Script", long_about = None)]
+
+
+struct Cli {
+    /// Spring target url
+    #[clap(short, long)]
+    url: String,
+
+    /// This mode for sending the payload
+    #[clap(short, long)]
+    deploy: bool,
+
+    /// This mode for accessing payload with interactive shell
+    #[clap(short, long)]
+    shell: bool,
+    
+    /// Input command to run in exploit
+    #[clap(short, long)]
+    cmd: Option<String>,
+
+}
+
 fn main() {
 	// Load the desired file into memory
 	let file_map = FileMap::open(r"C:\Program Files\7-Zip\7z.dll").unwrap();
-    
+    println!("Searching exports function in DLL to hijack");
 	// Process the image file
 	match PeFile::from_bytes(&file_map) {
 		Ok(Wrap::T32(file)) => dump_export32(file),
@@ -57,3 +94,8 @@ fn abort(message: &str) -> ! {
 	}
 	process::exit(1);
 }
+
+/*
+fn check_buildtool_exist(){
+	// cmd.exe /C '"C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\Common7\Tools\VsDevCmd.bat" && cl.exe'
+}*/
